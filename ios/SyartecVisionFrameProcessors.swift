0cc1d7549d9f430ee273ba6dc667548045aa1f5e
Syar-tech <51356918+Syar-tech@users.noreply.github.com>
2023-09-27 10:48:07 +0200

    fix: files

--- ios/SyartecVisionFrameProcessors.swift
+++
@@ -1,8 +0,0 @@
-@objc(SyartecVisionFrameProcessors)
-class SyartecVisionFrameProcessors: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
--- ios/SyartecVisionFrameProcessors/SyartecVisionFrameProcessors.swift
+++
@@ -1,9 +0,0 @@
-@objc(SyartecVisionFrameProcessorsPlugin)
-public class SyartecVisionFrameProcessorsPlugin: NSObject, FrameProcessorPluginBase {
-  @objc public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
-    let buffer = frame.buffer
-    let orientation = frame.orientation
-    // code goes here
-    return []
-  }
-}
